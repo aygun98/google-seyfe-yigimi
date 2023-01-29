dcba6cab04575a79bf16aa851496a08d4e2c2cd6
Aygun <aygun.xuduyeva98@gmail.com>
2023-01-29 02:05:16 -0800

    js-xmark

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const inputSide = document.querySelector('input')
 
 const xMark = document.querySelector('.fa-xmark')
 
-// let inputText;
+let inputText;
 
 inputSide.addEventListener('keyup' , silIconuGorunsun)
 
 
 function silIconuGorunsun(e) {
-    
-// inputText = inputSide.value
-if(inputSide.value.length >=1){
+    // e.preventDefault()
+    // console.log(e.key);
+inputText = inputSide.value
+if(inputText.length >= 1){
     xMark.style.display = 'block'
 }else{
     xMark.style.display = 'none'
 
 }
+  if(e.key == 'Enter'){
+    googleAxtar()
+  }
               //2ci varyant
 // if(inputText.length >=1){
 //     xMark.classList.add('goster')
@@ -26,13 +30,27 @@ if(inputSide.value.length >=1){
 
 }
 
+
+
 xMark.addEventListener('click', yazilarSilinsin)
  
-function yazilarSilinsin(e) {
+function yazilarSilinsin() {
     xMark.style.display = 'none'
-    inputSide.value = ' '
+    inputSide.value = ''
 }
 
+const axtarbtn = document.getElementById('searchbtn')
+axtarbtn.addEventListener('click', function () {
+    googleAxtar()
+} )   
 
-  
-   
+function googleAxtar() {
+    // e.preventDefault()
+    if (inputText.length >= 1) {
+        let url = `https://google.com/search?q=${inputText}`
+        window.location = url
+       
+    }
+ }
+
+ 
